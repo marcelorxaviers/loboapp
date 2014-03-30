61d61ba9e7c24c3e706936e45596eef2f5d79771
rhlobo <rhlobo+github@gmail.com>
2014-03-30 15:31:03 -0300

    Small adjustments.

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,45 @@ puts 'user: ' << user.email
 
 
 puts 'DEFAULT COMPANIES'
-company = Company.find_or_create_by_name :name => 'Sample A (dummy owner)', :user => user_dummy, :users => [user_dummy]
-puts 'company: ' << company.name
-company = Company.find_or_create_by_name :name => 'Sample B (admin as owner)', :user => user, :users => [user]
-puts 'company: ' << company.name
-company = Company.find_or_create_by_name :name => 'Sample C (dummy owner, admin as member)', :user => user_dummy, :users => [user_dummy, user]
-puts 'company: ' << company.name
+companyA = Company.find_or_create_by_name :name => 'Sample A (dummy owner)', :user => user_dummy, :users => [user_dummy]
+puts 'company: ' << companyA.name
+companyB = Company.find_or_create_by_name :name => 'Sample B (admin as owner)', :user => user, :users => [user]
+puts 'company: ' << companyB.name
+companyC = Company.find_or_create_by_name :name => 'Sample C (dummy owner, admin as member)', :user => user_dummy, :users => [user_dummy, user]
+puts 'company: ' << companyC.name
+
+
+puts 'DEFAULT CONTACTS'
+contact_a1 = Contact.find_or_create_by_email("aaa@nohere.com.bs") do |c|
+	c.first_name = "Astolfo" 
+	c.last_name = "Aguiar"
+	c.company_name = "AspasCorp"
+	c.job_title = "Auxiliar"
+	c.phone = ""
+	c.website = ""
+	c.company = companyA
+end
+companyA.contacts << contact_a1
+puts 'contact: ' << contact_a1.fullname
+contact_b1 = Contact.find_or_create_by_email("b1@nohere.com.bs") do |c|
+	c.first_name = "Brasticole" 
+	c.last_name = "Barrone"
+	c.company_name = "BravoCorp"
+	c.job_title = "Blasfemiador"
+	c.phone = ""
+	c.website = ""
+	c.company = companyB
+end
+companyB.contacts << contact_b1
+puts 'contact: ' << contact_b1.fullname
+contact_b2 = Contact.find_or_create_by_email("b2@nohere.com.bs") do |c|
+	c.first_name = "Bartolomeu" 
+	c.last_name = "Buscopanoide"
+	c.company_name = "Berscuponi"
+	c.job_title = "Bartender"
+	c.phone = ""
+	c.website = ""
+	c.company = companyB
+end
+companyB.contacts << contact_b2
+puts 'contact: ' << contact_b2.fullname
