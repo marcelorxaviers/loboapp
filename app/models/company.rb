7688ba476d39d93065770da5cd3254d774f382b3
rhlobo <rhlobo+github@gmail.com>
2014-04-03 09:33:14 -0300

    Some small adjustments (cleaning up things)

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,7 +4,7 @@ class Company < ActiveRecord::Base
   has_many :contacts, dependent: :destroy
   accepts_nested_attributes_for :contacts
 
-  validates_length_of :name, :minimum => 3, :maximum => 30, :allow_blank => false
+  validates :name, uniqueness: true, :length => { :minimum => 3, :maximum => 30 }, :allow_blank => false
   validates_length_of :highrise_token, :minimum => 30, :maximum => 35, :allow_blank => false
   validates_format_of :highrise_base_url, :with => URI.regexp
 end
