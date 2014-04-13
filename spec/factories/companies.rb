d094fd5ffa2975dddfff97de678b6e0c2b4c6414
rhlobo <rhlobo+github@gmail.com>
2014-04-13 19:32:25 -0300

    Model tests (both for company and contacts).

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -7,11 +7,4 @@ FactoryGirl.define do
     highrise_token "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
     user nil
   end
-
-  factory :company2 do
-    name "SAMPLE"
-    highrise_base_url "https://personal1788.highrisehq.com"
-    highrise_token "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
-    user nil
-  end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should have_and_belong_to_many(:users) }
+    it { should have_many(:contacts) }
+  end
+
+  describe "attribute validation" do
+    describe "name" do
+      it { should validate_uniqueness_of(:name) }
+      it { should ensure_length_of(:name).is_at_least(3).is_at_most(30) }
+
+      it { should_not allow_value("").for(:name) }
+
+      it { should allow_value("Empresa").for(:name) }
+      it { should allow_value("ACME").for(:name) }
+    end
+
+    describe "highrise_token" do
+      it { should ensure_length_of(:highrise_token).is_at_least(30).is_at_most(35) }
+      it { should_not allow_value("").for(:highrise_token) }
+
+      it { should allow_value("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE").for(:highrise_token) }
+      it { should allow_value("ee54dd83345c800c14b6fda0a650659e").for(:highrise_token) }
+    end
+
+    describe "highrise_base_url" do
+      it { should_not allow_value("").for(:highrise_base_url) }
+      it { should_not allow_value("personal1788.highrisehq.com").for(:highrise_base_url) }
+
+      it { should allow_value("https://personal1788.highrisehq.com").for(:highrise_base_url) }
+      it { should allow_value("http://xxx.highrise.com/blablablabla").for(:highrise_base_url) }
+    end
+  end
 end
